4d9793c3ee81b391bed192b350e5040c95642780
elgrian <anwar.loves.lacroix@gmail.com>
2019-01-26 23:49:47 -0800

    Prevented ball from trailing

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
 function draw() {
     //draw code
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
 }
 setInterval(draw, 10);
 
-ctx.beginPath();
-ctx.arc(50, 50, 10, 0, Math.PI*2);
-ctx.fillStyle = "#0095DD";
-ctx.fill();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.arc(x, y, 10, 0, Math.PI*2);
+// ctx.fillStyle = "#0095DD";
+// ctx.fill();
+// ctx.closePath();
+// x += dx;
+// y += dy;
