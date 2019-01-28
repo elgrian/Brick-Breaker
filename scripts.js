600a41ad1f0b4f808c2f0f638c9f16f2f3475464
elgrian <anwar.loves.lacroix@gmail.com>
2019-01-27 19:34:48 -0800

    Added variables for brick rows and columns

--- scripts.js
+++ scripts.js
@@ -11,6 +11,27 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+
+//brick row
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        vricks[c][r] = {x: 0, y: 0};
+    }
+}
+
+
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
